2578847e56e5e2b1efcf912ed1209356c59584e7
furkansalihipek <furkan@ipek.dev>
2025-03-12 17:13:02 +0300

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/gtyou-water-tech' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/gtyou-water-tech/' : '',
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig; 
